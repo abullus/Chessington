686d4d3a93328a63017a29a4bb143b7a836c96e8
abullus <67292876+abullus@users.noreply.github.com>
2020-07-13 15:00:12 +0100

    Added stop on how far pawns can move forward

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -14,24 +14,24 @@ namespace Chessington.GameEngine.Pieces
         {
             Square currentSquare = board.FindPiece(this);
             List<Square> potentialSquares = new List<Square>();
-            
+
+            int playerDirection = 0;
             if (this.Player == Player.White)
             {
-                potentialSquares.Add(Square.At(currentSquare.Row-1,currentSquare.Col));
-                if (this.HasMoved == false)
-                {
-                    potentialSquares.Add(Square.At(currentSquare.Row-2,currentSquare.Col));
-                }
-            }
-            if (this.Player == Player.Black)
-            {
-                potentialSquares.Add(Square.At(currentSquare.Row+1,currentSquare.Col));
-                if (this.HasMoved == false)
-                {
-                    potentialSquares.Add(Square.At(currentSquare.Row+2,currentSquare.Col));
-                }
+                playerDirection = -1;
+            }else {
+                playerDirection = 1;
             }
 
+            if (board.GetPiece(Square.At(currentSquare.Row + playerDirection, currentSquare.Col)) == null)
+            {
+                potentialSquares.Add(Square.At(currentSquare.Row+ playerDirection,currentSquare.Col));
+                if (this.HasMoved == false && board.GetPiece(Square.At(currentSquare.Row + playerDirection*2 ,currentSquare.Col))  == null)
+                {
+                    potentialSquares.Add(Square.At(currentSquare.Row+ playerDirection*2,currentSquare.Col));
+                }
+            }
+            
             return potentialSquares;
             //return potentialSquares.Where(square => board.GetPiece(square)  == null );
         }
